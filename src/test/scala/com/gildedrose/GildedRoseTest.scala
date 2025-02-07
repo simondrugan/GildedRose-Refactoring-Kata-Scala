6edc801b26ea629f113aade2b9f6c45ace301fa1
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2025-02-07 11:57:09 +0000

    test commit

--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class GildedRoseTest  extends AnyFlatSpec with Matchers {
-      it should "foo" in {
+      it should "foo bar" in {
         val items = Array[Item](new Item("foo", 0, 0))
         val app = new GildedRose(items)
         app.updateQuality()
